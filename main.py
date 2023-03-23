02cd1fee0f894194f1b5813fcd67de9b5ad60bd6
Cole Robertson <127897762+Robbo196475@users.noreply.github.com>
2023-03-23 21:21:40 +0000

    ive got my base code for my calculator

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 '''
-  Name: 
-  James Hargest College
-  Programming Internal for 2.7 & 2.8 ~ 12 credits
-  Due: 6 April 2023
-  
-  TIP: Use assessment guide to help guide you through this Internal
+Area
+
 '''
+# Heading 
+
+
+
+# Define function to calculate area of rectangle
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+# Define function to calculate perimeter of rectangle
+# Define function to calculate perimeter of rectangle
+def calculate_perimeter(length, width):
+    perimeter = 2 * (length + width)
+    return perimeter
+
+# Get user input for length and width of rectangle
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter width of rectangle: "))
+
+# Calculate and print area and perimeter of rectangle
+area = calculate_area(length, width)
+perimeter = calculate_perimeter(length, width)
+print("Area of rectangle: ", area)
+print("Perimeter of rectangle: ", perimeter)
